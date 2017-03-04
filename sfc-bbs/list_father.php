e4d6cd270631c86266afbd154e2af365e63dd25f
unknown <>
2017-03-04 22:54:18 +0800

    修改了图片路径

--- sfc-bbs/list_father.php
+++ sfc-bbs/list_father.php
@@ -91,7 +91,7 @@ $template['css'] = array('style/public.css','style/list.css');
 			<li>
 				<div class="smallPic">
 					<a href="member.php?id=<?php echo $data_content['member_id']?>" target="_blank">
-						<img width="45" height="45" src="<?php if($data_content['photo']!=''){echo $data_content['photo'];}else{echo 'style/photo.jpg';} ?>">
+						<img width="45" height="45" src="<?php if($data_content['photo']!=''){echo SUB_URL.$data_content['photo'];}else{echo 'style/photo.jpg';} ?>">
 					</a>
 				</div>
 				<div class="subject">
