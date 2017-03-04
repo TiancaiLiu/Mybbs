ee8ce2209bc5cb0e8b90815c2cd280f70daea1ce
unknown <>
2017-03-04 22:52:00 +0800

    增加了两个常量

--- sfc-bbs/inc/config.inc.php
+++ sfc-bbs/inc/config.inc.php
@@ -1,4 +1,5 @@
 <?php 
+date_default_timezone_get('Asia/Shanghai');
 session_start();
 header('Content-type:text/html;charset=utf-8');
 define('DB_HOST','localhost');
@@ -7,5 +8,9 @@ define('DB_PASSWORD','');
 define('DB_DATABASE','sfcbbs');
 define('DB_PORT',3306);
 
+//项目（程序）在服务器上的绝对路径
+define('SA_PATH',dirname(dirname(__FILE__)));
+//我们的项目在web根目录下面的位置（哪个目录里面）
+define('SUB_URL',str_replace($_SERVER['DOCUMENT_ROOT'],'',str_replace('\\','/',SA_PATH)).'/');
 
 ?>
